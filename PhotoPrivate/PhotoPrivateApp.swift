7edb23f8edce0f2d8c4f3083b28c06c27a0209bd
TeedsK <teedsthemyth@gmail.com>
2021-10-03 11:19:16 -0700

    Initial Commit

---
+++ PhotoPrivate/PhotoPrivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPrivateApp.swift
+//  PhotoPrivate
+//
+//  Created by Theo Kremer on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPrivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
